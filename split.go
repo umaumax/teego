7804d51abb424935102e31230e507bead663e700
umaumax <umauma7083@gmail.com>
2017-02-04 19:28:35 +0900

    fix CR bug2

--- split.go
+++ split.go
@@ -32,7 +32,7 @@ func ScanLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
 	if i := bytes.IndexByte(data, '\r'); i >= 0 {
 		// We have a full newline-terminated line.
 		//return i + 1, dropCR(data[0:i]), nil
-		return i + 1, data[0:i], nil
+		return i + 1, data[0 : i+1], nil
 	}
 	// If we're at EOF, we have a final, non-terminated line. Return it.
 	if atEOF {
