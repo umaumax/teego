3a42a356e977f982b9155b26137efaea7ce37dd1
umaumax <umauma7083@gmail.com>
2017-02-04 17:32:32 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	//	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+var (
+	fileViewFlag bool
+	stdViewFlag  bool
+	appendFlag   bool
+)
+
+func init() {
+	flag.BoolVar(&fileViewFlag, "file", false, "file view output flag")
+	flag.BoolVar(&stdViewFlag, "std", true, "stdout & stderr view output flag")
+	flag.BoolVar(&appendFlag, "b", false, "Append the output to the files rather than overwriting them.")
+	//	TODO
+	// -i      Ignore the SIGINT signal.
+}
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) == 0 {
+		fmt.Printf("no output filepath: %s filepath\n", strings.Join(os.Args, " "))
+		os.Exit(1)
+	}
+	name := args[0]
+	defaultModeFlag := os.O_CREATE | os.O_RDWR | os.O_TRUNC
+	appendModeFlag := os.O_CREATE | os.O_RDWR | os.O_APPEND
+	fileOpenModeFlag := defaultModeFlag
+	if appendFlag {
+		defaultModeFlag = appendModeFlag
+	}
+	fp, err := os.OpenFile(name, fileOpenModeFlag, 0644)
+	if err != nil {
+		fmt.Printf("input file create err:%s\n", err)
+		os.Exit(1)
+	}
+	defer fp.Close()
+
+	ansiContSeqReg := regexp.MustCompilePOSIX(`(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]`)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if fileViewFlag {
+			fmt.Fprintln(fp, text)
+		}
+		if stdViewFlag {
+			fmt.Fprintln(os.Stdout, text)
+		}
+
+		if fileViewFlag && stdViewFlag {
+			continue
+		}
+
+		//	remove "\r"
+		index := strings.LastIndex(text, "\r")
+		index += len("\r")
+		text = text[index:]
+		//	remove "ansiContSeqReg control sequences"
+		text = ansiContSeqReg.ReplaceAllString(text, "")
+
+		if !fileViewFlag {
+			fmt.Fprintln(fp, text)
+		}
+		if !stdViewFlag {
+			fmt.Fprintln(os.Stdout, text)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("stdin read err:%s\n", err)
+		os.Exit(1)
+	}
+}
